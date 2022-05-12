0b20815ddb56820c9fe81cd96bed02978543aa33
Francisco159 <javier.671634@gmail.com>
2022-05-12 11:29:55 -0500

    agregue la funcionalidad al login/register

--- Login and Register/script.js
+++ Login and Register/script.js
@@ -12,8 +12,47 @@ document.addEventListener('click', e => {
     }
 });
 
-document.addEventListener('click', e => {
-    if(e.target === $btnInicioSesion){
-        location.href = '../index.html';
-    }
-})
+var firebaseConfig = {
+    apiKey: "AIzaSyAlFQ056gA4YRWxVqix_LBQ73vXv2uFMMc",
+    authDomain: "pia-tm.firebaseapp.com",
+    projectId: "pia-tm",
+    storageBucket: "pia-tm.appspot.com",
+    messagingSenderId: "198957534463",
+    appId: "1:198957534463:web:3e47669630e88a6fac10ca"
+    };
+                               
+    // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+let user;
+
+function register() {
+    let email = document.getElementById('sign-up-email').value;
+    let password = document.getElementById('sign-up-passw').value;
+    
+
+    auth.createUserWithEmailAndPassword(email, password).then(function() {
+        user = document.getElementById('sign-up-name').value;
+        $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active');
+    }).catch(function (error) {
+        console.log(error);
+        alert("Error al crear usuario");
+    })
+}
+
+function login() {
+    let email = document.getElementById('sign-in-email').value;
+    let password = document.getElementById('sign-in-passw').value;
+    
+
+    auth.signInWithEmailAndPassword(email, password).then(function() {
+        window.location.href = '../index.html';
+        console.log('Usuario logeado');
+    }).catch(function (error) {
+        console.log(error);
+        alert('Error al iniciar sesión');
+    })
+}
